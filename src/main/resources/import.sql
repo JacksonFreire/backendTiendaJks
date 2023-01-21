1254915f8b0fda9c85d713e436c2a9c02e795ae3
Jackson Freire <jakofreire@gmail.com>
2023-01-21 01:36:04 +0100

    Conexion y adaptacion a la base de datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (1, 'Camiseta',25,'azul',1,'2023-10-02');
+INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (2, 'Pantalon',30,'rojo',2,'2023-10-02');
+INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (3, 'Medias',15,'verde',3,'2023-10-02');
+INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (4, 'Jean',58,'amarillo',4,'2023-10-02');
+INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (5, 'camisa',50,'negro',1,'2023-10-02');
