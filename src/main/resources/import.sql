bb083a1497d55d839f65331d187001b629999102
Jackson Freire <jakofreire@gmail.com>
2023-01-21 08:17:55 +0100

    agregar productos a db

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
-INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (1, 'Camiseta',25,'azul',1,'2023-10-02');
-INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (2, 'Pantalon',30,'rojo',2,'2023-10-02');
-INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (3, 'Medias',15,'verde',3,'2023-10-02');
-INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (4, 'Jean',58,'amarillo',4,'2023-10-02');
-INSERT INTO ventas (id, producto, precio,color, cantidad,create_at) VALUES (5, 'camisa',50,'negro',1,'2023-10-02');
+INSERT INTO ventas (id, producto, precio,color, cantidad,precio_total, create_at) VALUES (1, 'Camiseta',25,'azul',1,25,'2023-10-02',);
+INSERT INTO ventas (id, producto, precio,color, cantidad,precio_total,create_at) VALUES (2, 'Pantalon',30,'rojo',2,14,'2023-10-02');
+INSERT INTO ventas (id, producto, precio,color, cantidad,precio_total,create_at) VALUES (3, 'Medias',15,'verde',3,100,'2023-10-02');
+INSERT INTO ventas (id, producto, precio,color, cantidad,precio_total,create_at) VALUES (4, 'Jean',58,'amarillo',4,50,'2023-10-02');
+INSERT INTO ventas (id, producto, precio,color, cantidad,precio_total,create_at) VALUES (5, 'camisa',50,'negro',1,40,'2023-10-02');
+INSERT INTO productos (id, nombre, marca , precio,detalles, cantidad,create_at) VALUES (1, 'Camisa','DG',23, 'La camisa es elegante',2,'2023-09-11');
+INSERT INTO productos (id, nombre, marca , precio,detalles, cantidad,create_at) VALUES (2, 'Pantalon','Pervis',12, 'EL pantalon Jean',1,'2023-09-11');
+INSERT INTO productos (id, nombre, marca , precio,detalles, cantidad,create_at) VALUES (3, 'short','DG',45, 'Es de playa de coloro azul',4,'2023-09-11');
+INSERT INTO productos (id, nombre, marca , precio,detalles, cantidad,create_at) VALUES (4, 'Sombrero','Dynamics',13, 'La sombre es elegante',7,'2023-09-11');
