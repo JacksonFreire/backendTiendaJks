155427d7cd902eb2d227f2fd74cb3a4fdefab57b
JacksonFreire <jackson.freire.ruiz@gmail.com>
2023-01-22 21:31:27 +0100

    fix save ventas

--- src/main/java/com/tiendajks/tienda/services/VentaServiceImpl.java
+++ src/main/java/com/tiendajks/tienda/services/VentaServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.tiendajks.tienda.models.dao.IVentaDao;
 import com.tiendajks.tienda.models.entity.Venta;
@@ -16,26 +15,23 @@ public class VentaServiceImpl implements IVentaService{
 	private IVentaDao ventaDao;
 
 	@Override
-	@Transactional(readOnly = true)
 	public List<Venta> findAll() {
 		return (List<Venta>) ventaDao.findAll();
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public void save(Venta venta) {
+		venta.setPrecioTotal(venta.getCantidad() * venta.getPrecio());
 		ventaDao.save(venta);
 		
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public Venta findById(Long id) {
 		return ventaDao.findById(id).orElse(null);
 	}
 
 	@Override
-	@Transactional(readOnly = true)
 	public void delete(Venta venta) {
 		ventaDao.delete(venta);
 		
