07c5d413561f408c034f59bfb13184c6285a29ae
JacksonFreire <jackson.freire.ruiz@gmail.com>
2023-01-24 02:14:13 +0100

    cores for call external

--- src/main/java/com/tiendajks/tienda/controller/VentaController.java
+++ src/main/java/com/tiendajks/tienda/controller/VentaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tiendajks.tienda.models.entity.Venta;
 import com.tiendajks.tienda.services.IVentaService;
 
+@CrossOrigin(origins = { "http://localhost:4200", "*" })
 @RestController
 @RequestMapping("/api")
 public class VentaController {
